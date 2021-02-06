99cbf2547ac6685d50025a4c3b8dc16406d3121f
Wallace Augusto Pereira de Moraes <wallace-moraes@wallace.moraes>
2021-02-06 12:51:51 -0300

    Adicionando scripts de insert

--- src/main/resources/migration/version-202102051540__initial_script.sql
+++ src/main/resources/migration/version-202102051540__initial_script.sql
@@ -1,3 +1,5 @@
+drop table if exists pessoa;
+
 create table if not exists pessoa (
     id varchar(40) not null primary key,
     nome varchar(255),
---
+++ src/main/resources/migration/version-202102061220__insert_pessoa.sql
@@ -0,0 +1,11 @@
+insert into pessoa (id, nome, idade, nacionalidade)
+values (cast(uuid() as char), 'Wallace', 29, 'brasileiro');
+
+insert into pessoa (id, nome, idade, nacionalidade)
+values (cast(uuid() as char), 'Ana', 25, 'brasileira');
+
+insert into pessoa (id, nome, idade, nacionalidade)
+values (cast(uuid() as char), 'Sonia', 59, 'brasileira');
+
+insert into pessoa (id, nome, idade, nacionalidade)
+values (cast(uuid() as char), 'Deivid', 29, 'brasileiro');
